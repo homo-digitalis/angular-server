624b517f5343954a42bc1de2f5cc2bcb203b8447
homo-digitalis <homo-digitalis@gmx.de>
2019-02-24 21:01:52 +0100

    slimfast

--- dist/angular-server.js
+++ dist/angular-server.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
-const http = require("http");
 const https = require("https");
 const https_provider_1 = require("https-provider");
 const path = require("path");
@@ -15,39 +14,27 @@ class AngularServer {
             res.sendFile(`${completePathToAssets}/index.html`);
         });
     }
-    start(port, certificatesFileName) {
-        if (port === 443) {
-            try {
-                this.options = new https_provider_1.HTTPSProvider(certificatesFileName).provideHTTPSOptions();
-                this.httpsServer = https.createServer(this.options, this.expressApp);
-                this.httpsServer.listen(port);
-                this.info = `HTTPS Server is listening or port ${port}`;
-                this.expressApp.use("/*", require("redirect-https")({
-                    body: "<!-- Hello Mr Developer! Please use HTTPS instead -->",
-                }));
-            }
-            catch (error) {
-                this.info = "certificates?";
-            }
+    start(certificatesFileName) {
+        try {
+            this.options = new https_provider_1.HTTPSProvider(certificatesFileName).provideHTTPSOptions();
+            this.httpsServer = https.createServer(this.options, this.expressApp);
+            this.httpsServer.listen(443);
+            this.info = "HTTPS Server is listening or port 443";
+            this.expressApp.use("/*", require("redirect-https")({
+                body: "<!-- Hello Mr Developer! Please use HTTPS instead -->",
+            }));
         }
-        else {
-            this.httpServer = http.createServer(this.expressApp);
-            this.httpServer.listen(port);
-            this.info = `HTTP Server is listening or port ${port}`;
+        catch (error) {
+            this.info = `certificates? \n${error}`;
         }
         return this.info;
     }
 }
 exports.AngularServer = AngularServer;
-const angularServerPort = (process.env.PORT === undefined) ?
-    Number(process.argv[2]) :
-    Number(process.env.CHAT_SERVER_PORT);
 const hdPath = (process.env.HD_PATH === undefined) ?
-    "../dist/chat-frontend" :
+    "../chat-frontend" :
     process.env.HD_PATH;
-const nameOfCertificatesFile = (process.env.NAMEOF_CERT_FILE === undefined) ?
-    "angular-server-certificate" :
-    process.env.NAMEOF_CERT_FILE;
+const nameOfCertificatesFile = "angular-server-certificate";
 const angularServer = new AngularServer(hdPath);
 // tslint:disable-next-line:no-console
-console.log(angularServer.start(angularServerPort, nameOfCertificatesFile));
+console.log(angularServer.start(nameOfCertificatesFile));
